0a8516f65b35d98edaa7c3befebbf04def7085c0
Yves Seurynck <yves.seurynck@vives.be>
2019-11-18 14:01:00 +0100

    update

--- src/main/java/be/vives/ti/config/ApplicationConfiguration.java
+++ src/main/java/be/vives/ti/config/ApplicationConfiguration.java
@@ -1,11 +1,9 @@
 package be.vives.ti.config;
 
 import org.springframework.context.annotation.*;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 
 import java.io.File;
-import java.util.Properties;
 
 @Configuration
 @ComponentScan(value = "be.vives.ti", excludeFilters={
@@ -27,24 +25,6 @@ public class ApplicationConfiguration {
         return f;
     }
 
-    public JavaMailSenderImpl mailSender() {
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
-        Properties prop = new Properties();
-        prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.starttls.enable", true);
-        prop.put("mail.smtp.ssl.trust", "smtp.sendgrid.net");
-        prop.put("mail.smtp.quitwait", false);
-
-        javaMailSender.setJavaMailProperties(prop);
-
-        javaMailSender.setHost("smtp.sendgrid.net");
-        javaMailSender.setPort(587);
-        javaMailSender.setProtocol("smtp");
-        javaMailSender.setUsername("yves.seurynck");
-        javaMailSender.setPassword("8F3DFJTZhFN.!wW");
-
-        return javaMailSender;
-    }
 
 }
