036ffd0708c7f910316ffb354be041440b7b321c
Yves Seurynck <yves.seurynck@vives.be>
2020-10-19 11:38:21 +0200

    update bean validation

--- src/main/java/be/vives/ti/model/Student.java
+++ src/main/java/be/vives/ti/model/Student.java
@@ -1,8 +1,7 @@
 package be.vives.ti.model;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
-
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class Student {
